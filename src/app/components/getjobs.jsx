270aea361ec834b9d15993a3e27e3b6af12f519d
Gpu <142077517+kyubiner@users.noreply.github.com>
2025-07-23 09:17:24 +0700

    test 2

--- src/app/components/getjobs.jsx
+++ src/app/components/getjobs.jsx
@@ -1,4 +1,4 @@
 export default async function getJobs() {
-    const res = await fetch('http://localhost:3000/data/dummy.json')
+    const res = await fetch('http://127.0.0.1:3000/data/dummy.json')
     return res.json()
 }
